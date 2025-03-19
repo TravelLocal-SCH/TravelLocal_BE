bfba6412ccc6f994ee771eaf95e218fa77e4c995
kkt9253 <kkt9253@naver.com>
2025-03-19 17:07:53 +0900

    DOCS : 초기 설정

---
+++ src/main/java/sch/travellocal/TravelLocalApplication.java
@@ -0,0 +1,13 @@
+package sch.travellocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelLocalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelLocalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sch/travellocal/TravelLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package sch.travellocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelLocalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
