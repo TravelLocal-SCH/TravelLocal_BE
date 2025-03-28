c7ef653e846cd76caaae19e252c821b0911a6d39
kkt9253 <kkt9253@naver.com>
2025-03-29 00:46:07 +0900

    FEAT : 커스텀 DefaultOAuth2UserService & OAuth2User 구현
    - 로그인한 사용자의 기존 회원 여부에 따라 사용자 정보를 업데이트하거나 새로 저장
    - 최초 로그인 시, 권한을 GUIDE_CONSUMER 설정
    - 여행 프로그램 게시 이후, 사용자의 권한을 GUIDE_PROVIDER 변경

---
+++ src/main/java/sch/travellocal/common/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package sch.travellocal.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import sch.travellocal.auth.oauth.CustomOauth2UserService;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CustomOauth2UserService customOauth2UserService;
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(auth -> auth.disable());
+
+        http.logout(auth -> auth.disable());
+
+        http.formLogin(auth -> auth.disable());
+
+        http.httpBasic(auth -> auth.disable());
+
+        http.oauth2Login(oauth2 -> oauth2
+                .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
+                        .userService(customOauth2UserService))
+        );
+
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/").permitAll()
+                .anyRequest().authenticated()
+        );
+
+        http.sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        return http.build();
+    }
+
+}
