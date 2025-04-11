870b19ce1313b9524deddc65185945165ffe2254
kkt9253 <kkt9253@naver.com>
2025-04-11 17:39:06 +0900

    FEAT : 현재 로그인된 사용자 정보 조회 기능 추가
    - 인증 객체를 기반으로 DB에서 현재 로그인된 User 객체 조회

---
+++ src/main/java/sch/travellocal/domain/user/service/SecurityUserService.java
@@ -0,0 +1,29 @@
+package sch.travellocal.domain.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import sch.travellocal.common.exception.custom.ApiException;
+import sch.travellocal.common.exception.error.ErrorCode;
+import sch.travellocal.domain.user.entity.User;
+import sch.travellocal.domain.user.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class SecurityUserService {
+
+    private final UserRepository userRepository;
+
+    /**
+     * Jwt Token을 통해 로그인한 유저 가져오기
+     */
+    public User getUserByJwt() {
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new ApiException(ErrorCode.DATABASE_ERROR, "로그인에 성공하였지만 DB에 username이 존재하지 않습니다."));
+
+        return user;
+    }
+}
