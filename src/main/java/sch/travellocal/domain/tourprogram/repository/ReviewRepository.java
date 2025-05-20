a262007d9617ca58c6caad761454dd016db425ed
kkt9253 <kkt9253@naver.com>
2025-05-21 00:45:03 +0900

    FEAT & REFACTOR : 도메인 엔티티 추가 및 리팩토링
    - 누락된 필드 보완
    - 불필요한 쿼리 방지를 위한 Lazy전략 사용
    - @Builder 객체 생성 시 컬렉션필드가 null 초기화되는 문제 발생 -> @Builder.Default를 통해 빈 컬렉션 초기화
    - TourProgram:
       - addHashtag(): 양방향 연관관계 편의 메서드 추가
    - TourProgramSchedule: @UniqueConstraint로 유니크 제약 조건 추가
    - TourProgramCount: TourProgram의 review, wishlist에 대한 count 저장 테이블
    - TourProgramHashtag: TourProgram과 Hashtag 간 N:N 관계를 위한 중간 테이블

---
+++ src/main/java/sch/travellocal/domain/tourprogram/repository/ReviewRepository.java
@@ -0,0 +1,38 @@
+package sch.travellocal.domain.tourprogram.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import sch.travellocal.domain.tourprogram.dto.ReviewDto;
+import sch.travellocal.domain.tourprogram.dto.UserReviewDto;
+import sch.travellocal.domain.tourprogram.entity.TourProgram;
+import sch.travellocal.domain.tourprogram.entity.Review;
+import sch.travellocal.domain.user.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    @Query("SELECT new sch.travellocal.domain.tourprogram.dto.TourProgramReviewDto(" +
+            "r.user.id, r.user.name, r.id, r.rating, r.content, r.createdAt, r.updatedAt) " +
+            "FROM Review r " +
+            "WHERE r.tourProgram.id = :tourProgramId")
+    Page<ReviewDto> findReviewsByTourProgramId(@Param("tourProgramId") Long tourProgramId, Pageable pageable);
+
+    @Query("SELECT new sch.travellocal.domain.tourprogram.dto.UserReviewDto(" +
+            "r.tourProgram.id, r.tourProgram.title, r.id, r.rating, r.content, r.createdAt, r.updatedAt) " +
+            "FROM Review r " +
+            "WHERE r.user.id = :userId")
+    Page<UserReviewDto> findReviewsByUserId(@Param("userId") Long userId, Pageable pageable);
+
+    boolean existsByTourProgramAndUser(TourProgram tourProgram, User user);
+
+    Optional<Review> findByTourProgramIdAndUserId(Long tourProgramId, Long userId);
+
+    List<Review> findAllByTourProgramId(Long tourProgramId);
+}
---
+++ src/main/java/sch/travellocal/domain/tourprogram/repository/TourProgramRepository.java
@@ -0,0 +1,10 @@
+package sch.travellocal.domain.tourprogram.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import sch.travellocal.domain.tourprogram.entity.TourProgram;
+
+@Repository
+public interface TourProgramRepository extends JpaRepository<TourProgram, Long> {
+
+}
