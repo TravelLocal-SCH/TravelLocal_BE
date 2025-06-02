c02e6bbf5993413c94555079f9fa7f01da28de6b
kkt9253 <kkt9253@naver.com>
2025-06-03 04:28:51 +0900

    FEAT : 조건 기반 게시물 리스트 조회 API 추가
    - 해시태그와 지역 필터를 동적으로 처리하여 게시물 목록 조회
    - 정렬 옵션(생성일, 가격, 리뷰 수, 찜 수)에 따른 정렬 기능 포함
    - JPA Criteria API 사용

--- src/main/java/sch/travellocal/domain/tourprogram/repository/TourProgramRepository.java
+++ src/main/java/sch/travellocal/domain/tourprogram/repository/TourProgramRepository.java
@@ -1,10 +1,11 @@
 package sch.travellocal.domain.tourprogram.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import sch.travellocal.domain.tourprogram.entity.TourProgram;
 
 @Repository
-public interface TourProgramRepository extends JpaRepository<TourProgram, Long> {
+public interface TourProgramRepository extends JpaRepository<TourProgram, Long>, JpaSpecificationExecutor<TourProgram> {
 
 }
