b6affc23098bb710d12b6a094fa80d903e6088bf
kkt9253 <kkt9253@naver.com>
2025-09-08 00:07:54 +0900

    REFACTOR : 이미지 저장 로직 공통화를 위한 ImageService 추가
    배경: 여러 도메인에서 이미지 저장 코드가 중복 사용
    
    - 변경 사항
      - ImageTargetType(리뷰, 게시글 등)과 대상 Id를 파라미터로 받아, 어떤 도메인이든 범용적으로 이미지를 저장할 수 있도록 설계
      - 이미지 목록의 순서 보장을 위해 sequence 포함

--- src/main/java/sch/travellocal/upload/enums/ImageTargetType.java
+++ src/main/java/sch/travellocal/upload/enums/ImageTargetType.java
@@ -1,7 +1,8 @@
 package sch.travellocal.upload.enums;
 
 public enum ImageTargetType {
-    REVIEW,
-    TOUR_PROGRAM, // 현재는 썸네일 하나이기 때문에 TourProgram 자체에 종속시킴
-    USER_PROFILE
+    TOUR_PROGRAM_REVIEW,
+    TOUR_PROGRAM, // 현재는 썸네일 하나이기 때문에 현재는 TourProgram 자체에 종속시킴
+    USER_PROFILE,
+    PLACE_REVIEW,
 }
